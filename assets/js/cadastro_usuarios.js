0850056dd1a45593b24a39997793d3821c6b0277
João Pedro Santos <115579237+kolpzs@users.noreply.github.com>
2023-10-10 20:31:48 -0300

    Projeto Meninos do Lago V3.4
    falta tela de login e o resto

--- assets/js/cadastro_usuarios.js
+++ assets/js/cadastro_usuarios.js
@@ -24,10 +24,6 @@ nextBtn.addEventListener("click", ()=> {
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
 
-function myFunction() {
-   confirm("Cadastrado com Sucesso!");
-}
-
 function autoFill() {
     document.getElementById('user-name').value = "João Pedro";
     document.getElementById('user-lastname').value = "Pereira dos Santos";
@@ -54,4 +50,30 @@ function autoFill() {
     document.getElementById('user-medic').value = "Não";
     document.getElementById('emergency-name').value = "Fernanda Chini";
     document.getElementById('emergency-phone').value = 40028922;
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("send-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }

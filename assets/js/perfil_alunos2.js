d1e0dc7d478a1fd89f10a4e6643e66ffe6f1630e
João Pedro Santos <115579237+kolpzs@users.noreply.github.com>
2023-10-15 23:46:13 -0300

    Projeto Meninos do Lago V4

--- assets/js/perfil_alunos2.js
+++ assets/js/perfil_alunos2.js
@@ -1,11 +1,3 @@
-const x = document.querySelector(".ham");
-const y = document.querySelector(".nav-menu");
-
-x.addEventListener("click", () => {
-    x.classList.toggle('active');
-    y.classList.toggle('active');
-})
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
